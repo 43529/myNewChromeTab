e10b9cf45bc9f92812497123a03a92d28eeab15f
dongxiang39@cnpc.com <dongxiang39@cnpc.com>
2021-01-13 17:11:53 +0800

    添加链接功能，链接暂时无法正确跳转

--- js/addLinks.js
+++ js/addLinks.js
@@ -1,25 +1,35 @@
 // 函数定义
-let template = `<li class="link-item"><a></a></li>`
-function addLink() {
-    console.log('addlink func works!!');
-    renderTemplate();
-}
-function generateTemplate() {
-    let newLink = `<li class="link-item">新增链接</li>`
+let template = ``
+
+function generateTemplate(newLink) {
     template += newLink;
     console.log(template);
-    return template
+    return template;
 }
 function renderTemplate() {
-    document.getElementById("link-list").innerHTML = generateTemplate();
+    document.getElementById("link-list").innerHTML = template;
 }
 function showModal() {
-    document.getElementById("popup-background").setAttribute("style", "visibility:visible;")
+    document.getElementById("popup-background").setAttribute("style", "visibility:visible;");
 }
 function hideModal() {
-    document.getElementById("popup-background").setAttribute("style", "visibility:hidden;")
+    document.getElementById("popup-background").setAttribute("style", "visibility:hidden;");
+}
+function confirm() {
+    document.getElementById("popup-background").setAttribute("style", "visibility:hidden;");
+    let link = addNewLink();
+    generateTemplate(link);
+    renderTemplate();
+}
+function addNewLink() {
+    let link = document.querySelectorAll(".popup-content-item-input");
+    let linkName = link[0].value;
+    let linkUrl = link[1].value;
+    let newLink = `<li class="link-item"><a href="` + linkUrl + `">` + linkName + `</a></li>`
+    return newLink;
 }
 // 执行
-document.getElementById("link-list").innerHTML = template;
+renderTemplate();
 document.getElementById("add-link-button").addEventListener("click",showModal);
 document.getElementById("popup-content-button-cancle").addEventListener("click",hideModal);
+document.getElementById("popup-content-button-confirm").addEventListener("click",confirm);
