69df7940cb6b6554d3237c45c58f64ac919be371
dongxiang39@cnpc.com <dongxiang39@cnpc.com>
2021-01-15 14:08:29 +0800

    安装eslint, 调整代码规范

--- js/addLinks.js
+++ js/addLinks.js
@@ -1,35 +1,42 @@
 // 函数定义
-let template = ``
+let template = ``;
 
 function generateTemplate(newLink) {
-    template += newLink;
-    console.log(template);
-    return template;
+  template += newLink;
+  console.log(template);
+  return template;
 }
 function renderTemplate() {
-    document.getElementById("link-list").innerHTML = template;
+  document.getElementById('link-list').innerHTML = template;
 }
 function showModal() {
-    document.getElementById("popup-background").setAttribute("style", "visibility:visible;");
+  document.getElementById('popup-background')
+      .setAttribute('style', 'visibility:visible;');
 }
 function hideModal() {
-    document.getElementById("popup-background").setAttribute("style", "visibility:hidden;");
+  document.getElementById('popup-background')
+      .setAttribute('style', 'visibility:hidden;');
 }
 function confirm() {
-    document.getElementById("popup-background").setAttribute("style", "visibility:hidden;");
-    let link = addNewLink();
-    generateTemplate(link);
-    renderTemplate();
+  document.getElementById('popup-background')
+      .setAttribute('style', 'visibility:hidden;');
+  const link = addNewLink();
+  generateTemplate(link);
+  renderTemplate();
 }
 function addNewLink() {
-    let link = document.querySelectorAll(".popup-content-item-input");
-    let linkName = link[0].value;
-    let linkUrl = link[1].value;
-    let newLink = `<li class="link-item"><a href="` + linkUrl + `">` + linkName + `</a></li>`
-    return newLink;
+  const link = document.querySelectorAll('.popup-content-item-input');
+  const linkName = link[0].value;
+  const linkUrl = link[1].value;
+  const newLink = `
+    <li class="link-item"><a href="` + linkUrl + `">` + linkName + `</a></li>
+  `;
+  return newLink;
 }
 // 执行
 renderTemplate();
-document.getElementById("add-link-button").addEventListener("click",showModal);
-document.getElementById("popup-content-button-cancle").addEventListener("click",hideModal);
-document.getElementById("popup-content-button-confirm").addEventListener("click",confirm);
+document.getElementById('add-link-button').addEventListener('click', showModal);
+document.getElementById('popup-content-button-cancle')
+    .addEventListener('click', hideModal);
+document.getElementById('popup-content-button-confirm')
+    .addEventListener('click', confirm);
