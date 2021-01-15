ccdaede403dd8e7793b9b573a8e24ad92c813838
dongxiang39@cnpc.com <dongxiang39@cnpc.com>
2021-01-15 14:21:19 +0800

    使用prettier格式化

--- js/addLinks.js
+++ js/addLinks.js
@@ -3,40 +3,51 @@ let template = ``;
 
 function generateTemplate(newLink) {
   template += newLink;
+
   console.log(template);
   return template;
 }
 function renderTemplate() {
-  document.getElementById('link-list').innerHTML = template;
+  document.getElementById("link-list").innerHTML = template;
 }
 function showModal() {
-  document.getElementById('popup-background')
-      .setAttribute('style', 'visibility:visible;');
+  document
+    .getElementById("popup-background")
+    .setAttribute("style", "visibility:visible;");
 }
 function hideModal() {
-  document.getElementById('popup-background')
-      .setAttribute('style', 'visibility:hidden;');
+  document
+    .getElementById("popup-background")
+    .setAttribute("style", "visibility:hidden;");
 }
 function confirm() {
-  document.getElementById('popup-background')
-      .setAttribute('style', 'visibility:hidden;');
+  document
+    .getElementById("popup-background")
+    .setAttribute("style", "visibility:hidden;");
   const link = addNewLink();
   generateTemplate(link);
   renderTemplate();
 }
 function addNewLink() {
-  const link = document.querySelectorAll('.popup-content-item-input');
+  const link = document.querySelectorAll(".popup-content-item-input");
   const linkName = link[0].value;
   const linkUrl = link[1].value;
-  const newLink = `
-    <li class="link-item"><a href="` + linkUrl + `">` + linkName + `</a></li>
+  const newLink =
+    `
+    <li class="link-item"><a href="` +
+    linkUrl +
+    `">` +
+    linkName +
+    `</a></li>
   `;
   return newLink;
 }
 // 执行
 renderTemplate();
-document.getElementById('add-link-button').addEventListener('click', showModal);
-document.getElementById('popup-content-button-cancle')
-    .addEventListener('click', hideModal);
-document.getElementById('popup-content-button-confirm')
-    .addEventListener('click', confirm);
+document.getElementById("add-link-button").addEventListener("click", showModal);
+document
+  .getElementById("popup-content-button-cancle")
+  .addEventListener("click", hideModal);
+document
+  .getElementById("popup-content-button-confirm")
+  .addEventListener("click", confirm);
--- js/inputFile.js
+++ js/inputFile.js
@@ -1,28 +1,28 @@
-const changepic = function() {
+const changepic = function () {
   const reader = new FileReader();
-  f = document.getElementById('fileInput').files[0];
+  f = document.getElementById("fileInput").files[0];
   reader.readAsDataURL(f);
-  reader.onload = function(e) {
+  reader.onload = function (e) {
     localStorage.background = this.result;
     setBackgourndPic();
   };
 };
-const setBackgourndPic = function() {
+const setBackgourndPic = function () {
   if (localStorage.background) {
-    document.body.style.backgroundImage='url('+localStorage.background+')';
-    document.body.style.backgroundSize='100% 100%';
+    document.body.style.backgroundImage =
+      "url(" + localStorage.background + ")";
+    document.body.style.backgroundSize = "100% 100%";
   }
 };
-const search = function() {
-  const searchContent = document.getElementById('search-input').value;
+const search = function () {
+  const searchContent = document.getElementById("search-input").value;
   console.log(searchContent);
-  const href='https://www.baidu.com/s?word=' + searchContent;
+  const href = "https://www.baidu.com/s?word=" + searchContent;
   window.location.replace(href);
 };
 
 setBackgourndPic();
-document.getElementById('fileInput').onchange = function() {
+document.getElementById("fileInput").onchange = function () {
   changepic(this);
 };
-document.getElementById('search-button').onclick = search;
-
+document.getElementById("search-button").onclick = search;
