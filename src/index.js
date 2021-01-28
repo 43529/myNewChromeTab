644692e2ae8988e5620af2882a168312159c3efc
43529 <43529and43529@gmail.com>
2021-01-28 10:23:34 +0800

    安装webpack(因为使用webpack.config的时候浏览器报CSP错误，对webpack还不够熟练，所以暂时使用默认的打包方式

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import * as inputFile from "./js/inputFile.js"
+import * as addLinks from "./js/addLinks.js"
---
+++ src/js/inputFile.js
@@ -0,0 +1,28 @@
+const changePic = function () {
+  const reader = new FileReader();
+  let f = document.getElementById("fileInput").files[0];
+  reader.readAsDataURL(f);
+  reader.onload = function () {
+    localStorage.background = this.result;
+    setBackgroundPic();
+  };
+};
+const setBackgroundPic = function () {
+  if (localStorage.background) {
+    document.body.style.backgroundImage =
+      "url(" + localStorage.background + ")";
+    document.body.style.backgroundSize = "100% 100%";
+  }
+};
+const search = function () {
+  const searchContent = document.getElementById("search-input").value;
+  console.log(searchContent);
+  const href = "https://www.baidu.com/s?word=" + searchContent;
+  window.location.replace(href);
+};
+
+setBackgroundPic();
+document.getElementById("fileInput").onchange = function () {
+  changePic(this);
+};
+document.getElementById("search-button").onclick = search;
